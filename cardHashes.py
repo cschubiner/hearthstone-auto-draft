82c8fa12657fd31ce86ff3ef2f97231134223f53
Clay Schubiner <cschubiner@gmail.com>
2015-06-09 03:24:03 -0700

    manual corrections

--- cardHashes.py
+++ cardHashes.py
@@ -6,13 +6,29 @@ from downloadHashes import hashImage
 hashArr = list()
 m = dict()
 numToNameDict = dict()
+numToRarityDict = dict()
+manualHashNums = set()
+with open('manual_hashes.json','r') as j:
+    cardsJson = json.load(j)
+    for cardNum in cardsJson:
+        hsh = str(cardsJson[cardNum])
+        cardNum = int(cardNum)
+        manualHashNums.add(cardNum)
+        hashArr.append(hsh)
+        m[hsh] = cardNum
 with open('cardsWithHashes.json','r') as j:
     cardsJson = json.load(j)
     for c in cardsJson['cards']:
-        hashArr.append(c['imageHash'])
-        m[c['imageHash']] = c['id']
-        numToNameDict[c['id']] = c['name']
+        cardNum = c['id']
+        if cardNum not in manualHashNums:
+            hsh = c['imageHash']
+            hashArr.append(hsh)
+            m[hsh] = cardNum
+        numToNameDict[cardNum] = c['name']
+        numToRarityDict[cardNum] = c['quality']
 
+def imageNumToRarity(iNum):
+    return numToRarityDict[iNum]
 
 def imageNumToName(iNum):
     return numToNameDict[iNum]
@@ -60,37 +76,26 @@ def hamming_distance(s1, s2):
         raise ValueError("Undefined for sequences of unequal length")
     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
 
-def imageToCardName(original):
+def imageToCard(original):
     hashStr = hashImage(original)
 
     # print process.extract(hashStr, hashArr)
     # return m[process.extractOne(hashStr, hashArr)[0]]
 
-    # print hashStr
+    print hashStr
     scores = list()
     for h in hashArr:
         score = hamming_distance(hashStr, h)
         scores.append((score,h))
     scores = sorted(scores)
 
-    minVal = float('inf')
-    cardDir = 'hs-images/'
-
-    # print scores
-    minScore = float('inf')
-    bestImageNum = None
-    firstScore = scores[0][0]
     for hashScore, hsh in scores:
-        if hashScore > firstScore * 1.05:
-            break
         cardNum = m[hsh]
-        cardImage = cardDir + str(cardNum) + '.png'
-        score = compare_images(original, Image.open(cardImage))
-        if score < minScore:
-            minScore = score
-            bestImageNum = cardNum
-
-    return imageNumToName(bestImageNum)
+        ret = imageNumToName(cardNum), imageNumToRarity(cardNum)
+        print ret
+        if cardNum in manualHashNums and hashScore > 7:
+            continue
+        yield ret
 
 from PIL import Image
 import scipy
--- imageRecognize.py
+++ imageRecognize.py
@@ -2,14 +2,20 @@ import os
 import numpy
 from PIL import Image
 from downloadHashes import hashImage
-from cardHashes import imageToCardName
+from cardHashes import imageToCard
 import scipy
 import math, operator
 import scipy.misc
 import pyperclip
 import imagehash
-import time
 import json
+
+def isSameRarity(r1, r2):
+    if r1 == r2:
+        return True
+    a = sorted([r1, r2])
+    return a[0] == 'common' and a[1] == 'free'
+
 
 def callKMScript(cardName, scriptNum):
     pyperclip.copy(cardName)
@@ -19,7 +25,7 @@ def callKMScript(cardName, scriptNum):
 os.system("pngpaste 3cards.png")
 draftCards = Image.open('3cards.png')
 w, h = draftCards.size
-draftCards = draftCards.crop((515, 456, 1696, 594+348))
+draftCards = draftCards.crop((515, 455, 1696, 594+354))
 # scipy.misc.imsave('Dtest.png', draftCards)
 # exit()
 
@@ -27,13 +33,20 @@ dcw, dch = draftCards.size
 cardSize = int(.2948633 * float(dcw))
 
 spacing = (dcw - cardSize*3)/2
+firstRarity = None
 for i in range(3):
     w, h = draftCards.size
     cardImage = draftCards.crop((0 + (cardSize+spacing)*i, 0, (cardSize+spacing)*i + cardSize, h))
 
     scipy.misc.imsave('test' + str(i) + '.png', cardImage)
 
-    cardName = imageToCardName(cardImage)
-    print cardName
+    for cardName, rarity in imageToCard(cardImage):
+        if i == 0:
+            firstRarity = rarity
+            break
+        elif isSameRarity(firstRarity, rarity):
+            break
+
+
+    # print cardName, rarity,'\n'
     callKMScript(cardName, i)
-    time.sleep(1)
